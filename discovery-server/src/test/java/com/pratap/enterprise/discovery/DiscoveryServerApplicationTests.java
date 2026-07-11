ce154edbbbebd7b13be2c6af57b72b14e1746b8b
Ubuntu <ubuntu@ip-172-31-1-229.ap-south-1.compute.internal>
2026-07-11 14:44:09 +0000

    Add Eureka Discovery Server

---
+++ discovery-server/src/test/java/com/pratap/enterprise/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratap.enterprise.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
