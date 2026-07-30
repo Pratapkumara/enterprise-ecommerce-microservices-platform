fe02141c4d8b058fb42f309ccaeeb6aae2631d51
Pratap Kumar Sahoo <pratapsahoo772@gmail.com>
2026-07-30 03:58:09 +0000

    test: achieve full discovery server coverage

--- discovery-server/src/test/java/com/pratap/enterprise/discovery/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/pratap/enterprise/discovery/DiscoveryServerApplicationTests.java
@@ -1,13 +1,47 @@
 package com.pratap.enterprise.discovery;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mockStatic;
+
 class DiscoveryServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void applicationClassShouldBeInstantiable() {
+        DiscoveryServerApplication application =
+                new DiscoveryServerApplication();
 
+        assertNotNull(application);
+    }
+
+    @Test
+    void mainShouldStartSpringApplication() {
+        String[] args = {
+                "--spring.main.web-application-type=none"
+        };
+
+        try (
+                MockedStatic<SpringApplication> springApplication =
+                        mockStatic(SpringApplication.class)
+        ) {
+            springApplication
+                    .when(() -> SpringApplication.run(
+                            DiscoveryServerApplication.class,
+                            args
+                    ))
+                    .thenReturn(null);
+
+            DiscoveryServerApplication.main(args);
+
+            springApplication.verify(
+                    () -> SpringApplication.run(
+                            DiscoveryServerApplication.class,
+                            args
+                    )
+            );
+        }
+    }
 }
